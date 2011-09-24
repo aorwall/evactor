539d773a09944c7da965c8d5dd36b5a1ef1e56bb
Albert Örwall <hvnter@gmail.com>
2011-09-24 12:30:18 +0200

    Added counters

--- storage-cassandra/src/main/scala/se/aorwall/logserver/storage/CassandraStorage.scala
+++ storage-cassandra/src/main/scala/se/aorwall/logserver/storage/CassandraStorage.scala
@@ -7,12 +7,15 @@ import me.prettyprint.hector.api.Keyspace
 import me.prettyprint.cassandra.serializers.{StringSerializer, ObjectSerializer, UUIDSerializer, LongSerializer}
 import grizzled.slf4j.Logging
 import scala.collection.JavaConversions._
+import org.joda.time.DateTime
 
 class CassandraStorage(keyspace: Keyspace) extends LogStorage with Logging {
 
   val LOG_CF = "Log"
   val ACTIVITY_TIMELINE_CF = "ActivityTimeline"
   val ACTIVITY_CF = "Activity"
+  val ACTIVITY_STATE_CF = "ActivityState"
+  val ACTIVITY_COUNT_CF = "ActivityCount"
 
   def storeLog(activityId: String, log: Log): Unit = {
      val mutator = HFactory.createMutator(keyspace, StringSerializer.get);
@@ -54,18 +57,35 @@ class CassandraStorage(keyspace: Keyspace) extends LogStorage with Logging {
 
     // column family: Activity
     // row key: process id
-    // column key: activityId
-    // TODO: maybe set a secondary index on AtivityTimeline instead of having this cf for looking up activities?
-    mutator.insert(activity.processId, ACTIVITY_CF, HFactory.createColumn(activity.activityId, "", StringSerializer.get, StringSerializer.get))
+    // column key: activityId, ttl: 24 hours
+    mutator.insert(activity.processId, ACTIVITY_CF, HFactory.createColumn(activity.activityId, "", 3600*24, StringSerializer.get, StringSerializer.get))
 
-    // TODO: also save in state column family or something...?
+    // column family: ActivityState
+    // row key: process id + state
+    // column key: end timestamp
+    // value: activity id
+    mutator.insert(activity.processId + ":" + activity.state, ACTIVITY_STATE_CF, HFactory.createColumn(timeuuid, activity.activityId, UUIDSerializer.get, StringSerializer.get))
 
-    // TODO: ...and count
+    // column family: ActivityCount
+    // row key: process id + state + ["year";"month":"day":"hour"]
+    // column key: timestamp
+    // value: counter
+    // TODO: The reason why TimeUUIDUtils is used here is because I can't get LongSerializer to work. Needs to be fixed...
+    val time = new DateTime(activity.endTimestamp)
+    val count = new java.lang.Long(1L)
+    val year = new java.lang.Long(new DateTime(time.getYear, 1, 1, 0, 0).toDate.getTime)
+    val month = new java.lang.Long(new DateTime(time.getYear, time.getMonthOfYear, 1, 0, 0).toDate.getTime)
+    val day = new java.lang.Long(new DateTime(time.getYear, time.getMonthOfYear, time.getDayOfMonth, 0, 0).toDate.getTime)
+    val hour = new java.lang.Long(new DateTime(time.getYear, time.getMonthOfYear, time.getDayOfMonth, time.getHourOfDay, 0).toDate.getTime)
+    mutator.incrementCounter(activity.processId + ":" + activity.state + ":" + "year", ACTIVITY_COUNT_CF, year, count)
+    mutator.incrementCounter(activity.processId + ":" + activity.state + ":" + "month", ACTIVITY_COUNT_CF, month, count)
+    mutator.incrementCounter(activity.processId + ":" + activity.state + ":" + "day", ACTIVITY_COUNT_CF, day, count)
+    mutator.incrementCounter(activity.processId + ":" + activity.state + ":" + "hour", ACTIVITY_COUNT_CF, hour, count)
   }
 
   def readActivities(processId: String, fromTimestamp: Option[Long], count: Int ): List[Activity] = {
     val timeuuid = fromTimestamp match {
-      case Some(from) =>  TimeUUIDUtils.getTimeUUID(from)
+      case Some(from) => TimeUUIDUtils.getTimeUUID(from)
       case None => null
     }
 
