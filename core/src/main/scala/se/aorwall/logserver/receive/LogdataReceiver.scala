03a5aa415e3241db0308ffdeea671efaae701ec5
Albert Örwall <hvnter@gmail.com>
2011-09-24 15:14:35 +0200

    camel stuff in logdata receiver

--- core/src/main/scala/se/aorwall/logserver/receive/LogdataReceiver.scala
+++ core/src/main/scala/se/aorwall/logserver/receive/LogdataReceiver.scala
@@ -4,40 +4,37 @@ import akka.actor.{Actor}
 import grizzled.slf4j.Logging
 import se.aorwall.logserver.process.ProcessActor
 import se.aorwall.logserver.model.{Log}
-import akka.routing.{SmallestMailboxSelector, FixedSizeCapacitor, DefaultActorPool}
-import akka.actor.Actor._
 import akka.config.Supervision.OneForOneStrategy
+import akka.camel.{Message, Consumer}
 
 /**
  * 1. Receive log data object
  * 2. Save in DB
  * 3. Send to process actors
  */
-class LogdataReceiver extends Actor with Logging {
+class LogdataReceiver(camelUri: String) extends Actor with Consumer with Logging {
+
+  def endpointUri = camelUri
+
   self.faultHandler = new OneForOneStrategy(List(classOf[Throwable]), 5, 5000)
 
   def receive = {
-      case log: Log => processLogdata(log)
+    case msg: Message => processLogdata(getLogFromCamelMessage(msg))
+    case log: Log => processLogdata(log)
+  }
+
+  // TODO: This method should handle different data types in the message object
+  def getLogFromCamelMessage(msg: Message): Log = {
+    msg.bodyAs[Log]
   }
 
   def processLogdata(logevent: Log) = {
 
-    val monitoredProcesses = Actor.registry.actorsFor[ProcessActor] // TODO: Try using a listener to the ActorRegistry instead
+    val monitoredProcesses = Actor.registry.actorsFor[ProcessActor]
     debug("found " + monitoredProcesses.size + " process actors")
 
     // send logevent object to all process actors
-    for(processActor <- monitoredProcesses) processActor ! logevent
+    for (processActor <- monitoredProcesses) processActor ! logevent
   }
 
-}
-
-class LogdataReceiverPool extends Actor with DefaultActorPool
-                               with FixedSizeCapacitor
-                               with SmallestMailboxSelector
-{
-   def receive = _route
-   def limit = 5
-   def partialFill = true
-   def selectionCount = 1
-   def instance = actorOf[LogdataReceiver]
 }
