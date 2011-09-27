825e4d29d7f1af904023186d36d440e456fa52c5
Albert Örwall <hvnter@gmail.com>
2011-09-27 21:57:11 +0200

    Added dependency injection to be able to supervise typed actors

---
+++ core/src/main/scala/se/aorwall/logserver/microkernel/Boot.scala
@@ -0,0 +1,54 @@
+package se.aorwall.logserver.microkernel
+
+import se.aorwall.logserver.configuration.{ConfigurationServiceImpl, ConfigurationService}
+import akka.actor.Actor._
+import se.aorwall.logserver.receive.LogdataReceiver
+import se.aorwall.logserver.storage.invm.{InvmConfigurationStorage, InvmLogStorage}
+import akka.config.Supervision._
+import akka.actor.{Supervisor, TypedActor, SupervisorFactory}
+import akka.config.TypedActorConfigurator
+import com.google.inject.{Scopes, AbstractModule}
+import se.aorwall.logserver.storage.{LogStorage, ConfigurationStorage}
+
+/**
+ * Used to boot up in Akka Microkernel
+ *
+ * TODO: Read from config file
+ *
+ */
+class Boot {
+
+ val manager = new TypedActorConfigurator
+
+ manager
+   .addExternalGuiceModule(new AbstractModule() {
+      def configure {
+        bind(classOf[LogStorage]).to(classOf[InvmLogStorage]).in(Scopes.SINGLETON);
+        bind(classOf[ConfigurationStorage]).to(classOf[InvmConfigurationStorage]).in(Scopes.SINGLETON);
+      }})
+   .inject
+   .configure(
+    OneForOneStrategy(List(classOf[Exception]), 3, 1000),
+      Array(
+        new SuperviseTypedActor(
+          classOf[ConfigurationService],
+          classOf[ConfigurationServiceImpl],
+          Permanent,
+          1000)
+    ))
+   .supervise
+
+ val confService = manager.getInstance(classOf[ConfigurationService])
+
+ val factory = Supervisor(
+    SupervisorConfig(
+      OneForOneStrategy(List(classOf[Throwable]), 5, 5000),
+      Supervise(
+        actorOf(new LogdataReceiver("seda:logreceiver")).start,
+        Permanent) ::
+      Nil))
+
+ factory.start
+
+
+}
