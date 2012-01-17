31f15827cc41cb48c0634e97381544f1e5ee90ef
Albert Örwall <hvnter@gmail.com>
2012-01-17 22:06:11 +0100

    LogEventStorage

--- core/src/main/scala/se/aorwall/bam/storage/EventStorage.scala
+++ core/src/main/scala/se/aorwall/bam/storage/EventStorage.scala
@@ -3,7 +3,7 @@ package se.aorwall.bam.storage
 import se.aorwall.bam.model.events.Event
 
 trait EventStorage {
-
+  
   def storeEvent(event: Event): Boolean
   def readEvents(eventName: String, fromTimestamp: Option[Long], toTimestamp: Option[Long], count: Int, start: Int): List[Event]
   
